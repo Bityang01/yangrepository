4562ea44fd0a18ec8b380c563888e95ee4d56deb
Bityang01 <770545784@qq.com>
2019-09-17 20:46:28 +0800

    修改代码

--- Item/Item/main.c
+++ Item/Item/main.c
@@ -1,1 +1,87 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+typedef struct Node
+{
+	int data;
+	struct Node* next;
+}Node, *PList;
+void InitList(PList list)
+{
+	if (list->next == NULL)
+	{
+		return;
+	}
+	list->data = 0;
+	list->next = NULL;
+}
+Node *GetNode(int val)
+{
+	Node* pGet = (Node*)malloc(sizeof(Node));
+	pGet->data = val;
+	pGet->next = NULL;
+	return pGet;
+}
+void InsertHead(PList list, int val)
+{
+	Node* pGet = GetNode(val);
+	pGet->next = list->next;
+	list->next = pGet;
+}
+void Show(PList list)
+{
+	Node *pCur = list->next;
+	while (pCur != NULL)
+	{
+		printf("%d->", pCur->data);
+		pCur = pCur->next;
+	}
+	printf("NULL");
+	printf("\n");
+}
+void InsertTail(PList list, int val)
+{
+	Node* pCur = list->next;
+	while (pCur->next != NULL)
+	{
+		pCur = pCur->next;
+	}
+	Node* pGet = GetNode(val);
+	pCur->next = pGet;
+}
+int GetLength(PList list)
+{
+	Node *pCur = list->next;
+	int count = 0;
+	while (pCur != NULL)
+	{
+		count++;
+		pCur = pCur->next;
+	}
+	return count;
+}
+Node* Find(PList list, int val)
+{
+	Node* pCur = list->next;
+	int count = 1;
+	while (count != val)
+	{
+		pCur = pCur->next;
+		count++;
+	}
+	return pCur;
+}
+int main()
+{
+	Node list;
+	InitList(&list);
+	InsertHead(&list, 10);
+	InsertHead(&list, 20);
+	InsertHead(&list, 30);
+	InsertTail(&list, 40);
+	InsertTail(&list, 50);
+	InsertTail(&list, 60);
+	Node* pGet = Find(&list, 3);
+	printf("第%d个节点是%d\n", 3, pGet->data);
+	printf("%d\n", GetLength(&list));
+	Show(&list);
+}
