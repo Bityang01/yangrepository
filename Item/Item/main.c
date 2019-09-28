64d3052eefea7175163af3221ad210fe7b2502fb
Bityang01 <770545784@qq.com>
2019-09-28 21:08:46 +0800

    两个有序链表合并一个链表，按照从小到大输出

--- Item/Item/main.c
+++ Item/Item/main.c
@@ -654,7 +654,6 @@ char * toLowerCase(char * str){
 *     int val;
 *     struct ListNode *next;
 * };
-*/
 
 
 struct ListNode* middleNode(struct ListNode* head){
@@ -666,6 +665,32 @@ struct ListNode* middleNode(struct ListNode* head){
 		pFast = pFast->next->next;
 	}
 	return pSlow;
-}
+}*/
 
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+
+
+//两有序链表合并，合并链表从大到小输出
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+	if (l1 == NULL)
+		return l2;
+	if (l2 == NULL)
+		return l1;
+	if (l1->val<l2->val)
+	{
+		l1->next = mergeTwoLists(l1->next, l2);
+		return l1;
+	}
+	else{
+		l2->next = mergeTwoLists(l1, l2->next);
+		return l2;
+	}
+}
 
